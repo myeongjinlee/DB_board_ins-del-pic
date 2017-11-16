caeb2234498c07e383858c0a6611305029d99c15
doyeon <faithdy@kw.ac.kr>
2017-11-17 08:10:27 +0900

    MyPage View에 navBar로 탐색바 구현하였으며, 게시글들을 그리드로 구조화시킴. MyPage 렌더링 시 Model(MySQL DB) -> View 작업.

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+/* MySQL Variable */
+var mysql_dbc = require('./db/db_con')();
+var connection = mysql_dbc.init();
+
 /* 마이 페이지 */
 router.get('/', function(req, res, next) {
   var user_info = req.session.passport.user;
-  res.render('mypage', { user : user_info });
+
+  var first_query = 'select * from boards where ID = ? order by hit desc'; // 좋아요에 대한 쿼리문으로 변경하여야함!
+  connection.query(first_query, user_info.user_id, function (err, first_results) {
+    if(err) {
+      console.error('query error : ' + err);
+    } else {
+      var second_query = 'select * from boards where ID = ? order by NO asc';
+      connection.query(second_query, user_info.user_id, function (err, second_results) {
+        if(err) {
+          console.error('query error : ' + err);
+        } else {
+          res.render('mypage', {
+            user : user_info,
+            likedPosts : first_results,
+            writtenPosts : second_results,
+            categories : {}
+          });
+        }
+      })
+    }})
 });
 
 module.exports = router;
