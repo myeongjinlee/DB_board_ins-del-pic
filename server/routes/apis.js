ff2aa65020da1e2de4e74b4e96d3066c230a01c4
doyeon <faithdy@kw.ac.kr>
2017-11-19 02:08:16 +0900

    크롤링 모듈 추가 - 게시글 등록시에 크롤링 1.게시글 등록 api 변경 -> 메타 데이터를 db에 저장. 2.view에도 메타데이터가 나오도록 변경

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -5,6 +5,9 @@ var router = express.Router();
 var mysql_dbc = require('./db/db_con')();
 var connection = mysql_dbc.init();
 
+var request = require("request");
+var cheerio = require("cheerio");
+
 router.post('/createAccount', function(req, res, next) {
   var value = {
     ID            : req.body.id,
@@ -24,39 +27,51 @@ router.post('/createAccount', function(req, res, next) {
 });
 
 router.post('/writePost', function (req, res, next) {
-  var value = {
-    ID            : req.session.passport.user.user_id,
-    Title         : req.body.Title,
-    Content       : req.body.Content,
-    URL           : req.body.URL
-  };
+  request(req.body.URL, function (err, req_res, html) {
+    if(err) throw err;
 
-  var HashTags = req.body.HashTags;
-  HashTags = HashTags.split(',');
+    var $ = cheerio.load(html);
+    var title = $('meta[property="og:title"]').attr('content');
+    var content = $('meta[property="og:description"]').attr('content');
+    var image = $('meta[property="og:image"]').attr('content');
 
-  var first_query = 'insert into BOARDS SET NO = NULL, ?, Hit = 0, Create_date = now()';
-  connection.query(first_query, value, function (err, result) {
-    if(err) {
-      console.log('err :' + err);
-      res.send('<script>alert("등록 중 에러가 발생하였습니다.");location.href="/account/writePost";</script>');
-    } else {
-      var NO = result.insertId;
-      var second_query = 'insert into HASHTAGS(NO,HashTag) VALUES';
-      for(var i=0; i<HashTags.length; i++) {
-        second_query += '(' + NO + ',' + '"' + HashTags[i] + '"' + ')';
-        if(i!=HashTags.length-1) second_query +=',';
-      } second_query += ';';
-
-      connection.query(second_query,function (err, result) {
-        if(err) {
-          console.log('err :' + err);
-          res.send('<script>alert("등록 중 에러가 발생하였습니다.");location.href="/account/writePost";</script>');
-        } else {
-          console.log('게시글 등록이 완료되었습니다.');
-          res.send('<script>alert("게시글 등록이 완료되었습니다.");location.href="/account/";</script>');
-        }
-      })
+    var value = {
+      ID            : req.session.passport.user.user_id,
+      Title         : req.body.Title,
+      Content       : req.body.Content,
+      URL           : req.body.URL,
+      MetaTitle     : title,
+      MetaContent   : content,
+      MetaImage     : image
     }
+
+    var HashTags = req.body.HashTags;
+    HashTags = HashTags.split(',');
+
+    var first_query = 'insert into BOARDS SET NO = NULL, Hit = 0, Create_date = now(), ?';
+    connection.query(first_query, value, function (err, result) {
+      if(err) {
+        console.log('err :' + err);
+        res.send('<script>alert("등록 중 에러가 발생하였습니다.");location.href="/account/writePost";</script>');
+      } else {
+        var NO = result.insertId;
+        var second_query = 'insert into HASHTAGS(NO,HashTag) VALUES';
+        for(var i=0; i<HashTags.length; i++) {
+          second_query += '(' + NO + ',' + '"' + HashTags[i] + '"' + ')';
+          if(i!=HashTags.length-1) second_query +=',';
+        } second_query += ';';
+
+        connection.query(second_query,function (err, result) {
+          if(err) {
+            console.log('err :' + err);
+            res.send('<script>alert("등록 중 에러가 발생하였습니다.");location.href="/account/writePost";</script>');
+          } else {
+            console.log('게시글 등록이 완료되었습니다.');
+            res.send('<script>alert("게시글 등록이 완료되었습니다.");location.href="/account/";</script>');
+          }
+        })
+      }
+    })
   })
 })
 
